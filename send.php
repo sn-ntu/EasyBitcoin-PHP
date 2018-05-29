84e8be48d3409f6c733a5eb540318e9fd45ee2f0
sn-ntu <snguyen.ntu@gmail.com>
2018-05-30 00:09:51 +0700

    Fixed sendtoaddress

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 require_once('easybitcoin.php');
 if (isset($_POST['addr'])){
     $bitcoin = new Bitcoin('username', 'password', 'localhost', '8332');
-    $bitcoin->sendtoaddr($_POST['addr'], 10);
+    $bitcoin->sendtoaddress($_POST['addr'], 10, "Zcoin testnet faucet");
 } else{
     echo "invalid operation";
 }
