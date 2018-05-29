32abc7a90d0504f6fb7b7dca212502d9ab9e4b93
sn-ntu <snguyen.ntu@gmail.com>
2018-05-29 21:27:54 +0700

    Updated POST params

--- send.php
+++ send.php
@@ -1,9 +1,8 @@
 <?php
-	require_once('easybitcoin.php');
-	if(isset($_POST['addr'])){
-		$bitcoin = new Bitcoin('username','password','localhost','8332');
-		$bitcoin->sendtoaddr($_POST['addr'], 10);
-	}else{
-		echo "invalide operation";
-	}
+require_once('easybitcoin.php');
+if (isset($_POST['addr'])){
+    $bitcoin = new Bitcoin('username', 'password', 'localhost', '8332');
+    $bitcoin->sendtoaddr($_POST['addr'], 10);
+} else{
+    echo "invalid operation";
 }
